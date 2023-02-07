666c34715ae2f4ef59d2e5cb1953a6ef4ec3dd59
Raghvendra Sahu <rx.sahu@gmail.com>
2023-02-07 19:40:04 +0530

    array demo created

--- src/com/google/array/ArrayDemo.java
+++ src/com/google/array/ArrayDemo.java
@@ -9,11 +9,20 @@ public class ArrayDemo {
 		// 1. 
 		int[] numbers = new int[5];
 		//2. 
-		int[] numbers2 = new int[] {1,2,3,4,5};
+		int[] numbers2 = new int[] {1,2,3,4,5,7,8,9};
 		//3. 
 		int[] numbers3;
 		numbers3 = new int[] {1,2,3};
 		numbers3 = new int[4];
+		
+		for(int i : numbers2) {
+			System.out.println(i);
+		}
+		numbers2[0] = 100;
+		
+		for(int i : numbers2) {
+			System.out.println(i);
+		}
 		
 		
 	}
---
+++ src/com/google/array/StringArrrayDemo.java
@@ -0,0 +1,12 @@
+package com.google.array;
+
+public class StringArrrayDemo {
+
+	public static void main(String[] args) {
+
+		String[] names = new String[50];
+		names[0] =  "Ram";
+		names[4] = "Shyam";
+	}
+
+}
