36caeaf4c2bd0590490525cb2978fd038cc3b54d
Raghvendra Sahu <rx.sahu@gmail.com>
2023-02-07 19:19:21 +0530

    created a demo class for the arrays

--- src/com/google/array/ArrayDemo.java
+++ src/com/google/array/ArrayDemo.java
@@ -3,4 +3,18 @@ package com.google.array;
 public class ArrayDemo {
 
 	
+	public static void main(String[] args) {
+
+		// ways to create the array 
+		// 1. 
+		int[] numbers = new int[5];
+		//2. 
+		int[] numbers2 = new int[] {1,2,3,4,5};
+		//3. 
+		int[] numbers3;
+		numbers3 = new int[] {1,2,3};
+		numbers3 = new int[4];
+		
+		
+	}
 }
