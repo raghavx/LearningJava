7787bbc8617c413229335dcefad4fdd22355705d
Raghvendra Sahu <rx.sahu@gmail.com>
2023-02-07 19:45:00 +0530

    for loop in the string array

--- src/com/google/array/StringArrrayDemo.java
+++ src/com/google/array/StringArrrayDemo.java
@@ -7,6 +7,10 @@ public class StringArrrayDemo {
 		String[] names = new String[50];
 		names[0] =  "Ram";
 		names[4] = "Shyam";
+		
+		for(String name : names) {
+			System.out.println(name);
+		}
 	}
 
 }
